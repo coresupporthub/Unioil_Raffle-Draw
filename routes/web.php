fb439bca3a589c39aeb376ee8fd307d1cc983a6f
Tish <tbtizon.chmsu@gmail.com>
2024-11-13 23:08:21 +0800

    layout update

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Admin.index');
 });
+
+Route::get('/blank', function () {
+    return view('Admin.blank');
+});
+
