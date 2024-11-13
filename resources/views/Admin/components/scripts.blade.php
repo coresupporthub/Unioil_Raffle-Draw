9aa46af3c554216dbcf18de490675d658719ba60
Tish <tbtizon.chmsu@gmail.com>
2024-11-14 02:25:02 +0800

    added datatable scripts

--- resources/views/Admin/components/scripts.blade.php
+++ resources/views/Admin/components/scripts.blade.php
@@ -7,6 +7,26 @@
     <script src="{{asset('./dist/js/tabler.min.js?1692870487')}}" defer></script>
     <script src="{{asset('./dist/js/demo.min.js?1692870487')}}" defer></script>
     
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            const list = new List('table-default', {
+                sortClass: 'table-sort',
+                listClass: 'table-tbody',
+                valueNames: ['sort-name', 'sort-type', 'sort-city', 'sort-score',
+                    {
+                        attr: 'data-date',
+                        name: 'sort-date'
+                    },
+                    {
+                        attr: 'data-progress',
+                        name: 'sort-progress'
+                    },
+                    'sort-quantity'
+                ]
+            });
+        })
+    </script>
+    
     {{-- <script>
         // @formatter:off
         document.addEventListener("DOMContentLoaded", function() {
--- resources/views/Admin/customers.blade.php
+++ resources/views/Admin/customers.blade.php
@@ -101,26 +101,6 @@
         </div>
     </div>
 
-    <script>
-        document.addEventListener("DOMContentLoaded", function() {
-            const list = new List('table-default', {
-                sortClass: 'table-sort',
-                listClass: 'table-tbody',
-                valueNames: ['sort-name', 'sort-type', 'sort-city', 'sort-score',
-                    {
-                        attr: 'data-date',
-                        name: 'sort-date'
-                    },
-                    {
-                        attr: 'data-progress',
-                        name: 'sort-progress'
-                    },
-                    'sort-quantity'
-                ]
-            });
-        })
-    </script>
-
     @include('Admin.components.scripts')
 
 </body>
