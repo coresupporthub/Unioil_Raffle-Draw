6b90d25374db81eec515c84697d1ab0e7f90319f
RheyanJohn <rjvblanco.chmsu@gmail.com>
2024-11-14 00:11:40 +0800

    Add Model and Migration script for

---
+++ database/migrations/2024_11_13_155838_create_qr_codes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('qr_codes', function (Blueprint $table) {
+            $table->uuid('qr_id')->primary();
+            $table->string('code');
+            $table->string('entry_type');
+            $table->enum('status', ['used', 'unused']);
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('qr_codes');
+    }
+};
