db718cecb0332e012398c3ede0b8abc181750c81
Tish <tbtizon.chmsu@gmail.com>
2024-11-14 00:09:06 +0800

    admin layout format

--- resources/views/Customer/components/header.blade.php
+++ resources/views/Customer/components/header.blade.php
@@ -7,7 +7,7 @@
             </button>
             <h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
                 <a href=".">
-                    <img src="./static/logo.svg" width="110" height="32" alt="Tabler"
+                    <img src="{{asset('unioil_images/unioil_logo.png')}}" width="110" height="32" alt="Unioil"
                         class="navbar-brand-image">
                 </a>
             </h1>
--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
+//ADMIN SIDE
 Route::get('/', function () {
     return view('Admin.index');
-});
+})->name('index');
 
 Route::get('/blank', function () {
     return view('Admin.blank');
-});
+})->name('blank');
 
+Route::get('/QR/Generator', function () {
+    return view('Admin.qrgenerator');
+})->name('qrgenerator');
+
+
+
+
+//CUSTOMER SIDE
 Route::get('/Registration/Page', function () {
     return view('Customer.registration');
 });
+
