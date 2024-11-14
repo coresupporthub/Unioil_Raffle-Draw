f529585ed5259bf31b685c02bbe1af94d33c6045
RheyanJohn <info@coresupporthub.com>
2024-11-14 21:35:53 +0800

    Update Comments

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,11 @@ Route::group(['middleware' => ['web']], function () {
     Route::post('/generate-qr-code', [QrCodeController::class, 'generate']);
     Route::get('/get-qr-code-generated', [QrCodeController::class, 'getqrcodegenerated']);
     Route::post('/delete-generate-qr-code', [QrCodeController::class, 'deletegeneratedqr']);
+    Route::get('/get-queue-status', [QrCodeController::class, 'queueProgress']);
+    Route::post('/export-qr', [QrCodeController::class, 'exportQR']);
+
+    //Authentication
+    Route::post('/admin/auth', [AuthenticationController::class, 'signin']);
 
     // Retail Store Controller
     Route::post('/add-retail-store', [RetailStoreController::class, 'addcluster']);
@@ -20,10 +25,5 @@ Route::group(['middleware' => ['web']], function () {
     Route::post('/add-region', [RetailStoreController::class, 'addregion']);
     Route::post('/get-region-by-cluster', [RetailStoreController::class, 'getregionbycluster']);
     Route::post('/add-city', [RetailStoreController::class, 'addcity']);
-
-    Route::get('/get-queue-status', [QrCodeController::class, 'queueProgress']);
-    Route::post('/export-qr', [QrCodeController::class, 'exportQR']);
-
-    Route::post('/admin/auth', [AuthenticationController::class, 'signin']);
 
 });
