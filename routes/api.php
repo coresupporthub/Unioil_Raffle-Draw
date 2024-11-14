d946b286b6be1dfae0519c988ac61d18aebb7cda
RheyanJohn <info@coresupporthub.com>
2024-11-15 01:48:04 +0800

    Add Authentication Middleware for all authenticated api

--- routes/api.php
+++ routes/api.php
@@ -5,15 +5,33 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QrCodeController;
 use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\RetailStoreController;
+use App\Http\Middleware\ApiAuthentication;
 
 
 Route::group(['middleware' => ['web']], function () {
 
-    Route::post('/generate-qr-code', [QrCodeController::class, 'generate']);
-    Route::get('/get-qr-code-generated', [QrCodeController::class, 'getqrcodegenerated']);
-    Route::post('/delete-generate-qr-code', [QrCodeController::class, 'deletegeneratedqr']);
-    Route::get('/get-queue-status', [QrCodeController::class, 'queueProgress']);
-    Route::post('/export-qr', [QrCodeController::class, 'exportQR']);
+    
+    Route::middleware([ApiAuthentication::class])->group(function () {
+
+        Route::post('/generate-qr-code', [QrCodeController::class, 'generate']);
+        Route::get('/get-qr-code-generated', [QrCodeController::class, 'getqrcodegenerated']);
+        Route::post('/delete-generate-qr-code', [QrCodeController::class, 'deletegeneratedqr']);
+        Route::get('/get-queue-status', [QrCodeController::class, 'queueProgress']);
+        Route::post('/export-qr', [QrCodeController::class, 'exportQR']);
+
+
+            // Retail Store Controller
+        Route::post('/add-retail-store', [RetailStoreController::class, 'addcluster']);
+        Route::get('/get-cluster', [RetailStoreController::class, 'getcluster']);
+        Route::post('/cluster-status', [RetailStoreController::class, 'clusterstatus']);
+        Route::post('/add-region', [RetailStoreController::class, 'addregion']);
+        Route::post('/get-region-by-cluster', [RetailStoreController::class, 'getregionbycluster']);
+        Route::post('/add-city', [RetailStoreController::class, 'addcity']);
+        Route::post('/get-city-by-region', [RetailStoreController::class, 'getcitybyregion']);
+        Route::post('/add-store', [RetailStoreController::class, 'addstore']);
+        Route::get('/get-region', [RetailStoreController::class, 'getallregion']);
+        Route::get('/get-city', [RetailStoreController::class, 'getallcity']);
+    });
 
     //Authentication
     Route::post('/admin/auth', [AuthenticationController::class, 'signin']);
@@ -22,16 +40,6 @@ Route::group(['middleware' => ['web']], function () {
     Route::post('/logout', [AuthenticationController::class, 'logout']);
     Route::post('/resend-code', [AuthenticationController::class, 'resendcode']);
 
-    // Retail Store Controller
-    Route::post('/add-retail-store', [RetailStoreController::class, 'addcluster']);
-    Route::get('/get-cluster', [RetailStoreController::class, 'getcluster']);
-    Route::post('/cluster-status', [RetailStoreController::class, 'clusterstatus']);
-    Route::post('/add-region', [RetailStoreController::class, 'addregion']);
-    Route::post('/get-region-by-cluster', [RetailStoreController::class, 'getregionbycluster']);
-    Route::post('/add-city', [RetailStoreController::class, 'addcity']);
-    Route::post('/get-city-by-region', [RetailStoreController::class, 'getcitybyregion']);
-    Route::post('/add-store', [RetailStoreController::class, 'addstore']);
-    Route::get('/get-region', [RetailStoreController::class, 'getallregion']);
-    Route::get('/get-city', [RetailStoreController::class, 'getallcity']);
+
 
 });
--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,38 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\AuthMiddleware;
 //ADMIN SIDE
 
 //NAVIGATION
-Route::get('/', function () {
-    return view('Admin.index');
-})->name('index');
-Route::get('/blank', function () {
-    return view('Admin.blank');
-})->name('blank');
-Route::get('/qr/generator', function () {
-    return view('Admin.qrgenerator');
-})->name('qrgenerator');
-Route::get('/coupon/management', function () {
-    return view('Admin.managecoupon');
-})->name('managecoupon');
-Route::get('/retail/outlets', function () {
-    return view('Admin.retailoutlets');
-})->name('retailoutlets');
-Route::get('/customer/registrations', function () {
-    return view('Admin.customers');
-})->name('customers');
 
-//SETTINGS
-Route::get('/account/settings', function () {
-    return view('Admin.accountsettings');
-})->name('accountsettings');
+Route::middleware([AuthMiddleware::class])->group(function () {
+    Route::get('/', function () {
+        return view('Admin.index');
+    })->name('index');
+    Route::get('/blank', function () {
+        return view('Admin.blank');
+    })->name('blank');
+    Route::get('/qr/generator', function () {
+        return view('Admin.qrgenerator');
+    })->name('qrgenerator');
+    Route::get('/coupon/management', function () {
+        return view('Admin.managecoupon');
+    })->name('managecoupon');
+    Route::get('/retail/outlets', function () {
+        return view('Admin.retailoutlets');
+    })->name('retailoutlets');
+    Route::get('/customer/registrations', function () {
+        return view('Admin.customers');
+    })->name('customers');
+
+    //SETTINGS
+    Route::get('/account/settings', function () {
+        return view('Admin.accountsettings');
+    })->name('accountsettings');
+
+});
+
 
 //AUTHENTICATION
 Route::get('/admin/sign-in', function () {
