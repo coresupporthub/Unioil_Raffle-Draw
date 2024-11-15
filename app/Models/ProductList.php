d8aadfb52fd5b70272fd03fd9960795e251d8446
RheyanJohn <info@coresupporthub.com>
2024-11-15 20:57:12 +0800

    Add Product table and seeders

---
+++ app/Models/ProductList.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductList extends Model
+{
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $table = 'product_lists';
+    protected $primaryKey = 'product_id';
+    protected $fillable = [
+        'product_name',
+        'product_type',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->product_id)) {
+                $model->product_id = (string) \Illuminate\Support\Str::uuid();
+            }
+        });
+    }
+}
