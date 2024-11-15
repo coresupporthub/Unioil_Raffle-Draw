0d096fcaa095b4ccfa01315cff493a9340bc3843
RheyanJohn <info@coresupporthub.com>
2024-11-15 16:42:20 +0800

    Add 404 error fallback page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<style>
+
+@import url("https://fonts.googleapis.com/css?family=Fira+Code&display=swap");
+* {
+  margin: 0;
+  padding: 0;
+  font-family: "Fira Code", monospace;
+}
+
+body {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #ecf0f1;
+}
+
+.container {
+  text-align: center;
+  margin: auto;
+  padding: 4em;
+}
+.container img {
+  width: 256px;
+  height: 225px;
+}
+.container h1 {
+  margin-top: 1rem;
+  font-size: 35px;
+  text-align: center;
+}
+.container h1 span {
+  font-size: 60px;
+}
+.container p {
+  margin-top: 1rem;
+}
+.container p.info {
+  margin-top: 4em;
+  font-size: 12px;
+}
+.container p.info a {
+  text-decoration: none;
+  color: rgb(84, 84, 206);
+}/*# sourceMappingURL=404.css.map */
+
+</style>
+<body>
+    <div class="container">
+        <img src="/unioil_images/404.png" style="width: 100%" />
+
+        <h1>
+            Page is not Found
+        </h1>
+        <p>This page is not available</p>
+      </div>
+</body>
+</html>
