4de16f3870cdd28b8d7711892a74f5cfa92e1a34
RheyanJohn <info@coresupporthub.com>
2024-11-16 20:21:37 +0800

    Add Front end for upload CSV

--- resources/views/Admin/retailoutlets.blade.php
+++ resources/views/Admin/retailoutlets.blade.php
@@ -22,7 +22,19 @@
                     <!-- Page title actions -->
                     <div class="col-auto ms-auto d-print-none">
                       <div class="btn-list">
-                        <button class="btn btn-primary d-none d-sm-inline-block" data-bs-toggle="modal" data-bs-target="#modal-simple">
+                        <button class="btn btn-primary d-none d-sm-inline-block" data-bs-toggle="modal" data-bs-target="#importCsvModal">
+                            <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-file-description">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+                                <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
+                                <path d="M9 17h6" />
+                                <path d="M9 13h6" />
+                              </svg>
+                           Import CSV Data
+                          </button>
+
+                        <button class="btn btn-primary d-none d-sm-inline-block" data-bs-toggle="modal" data-bs-target="#addClusterModal">
                           <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
                           <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg>
                           Add Outlet
@@ -54,10 +66,10 @@
                                                 </tr>
                                             </thead>
                                             <tbody class="table-tbody">
-                                                
+
                                             </tbody>
                                         </table>
-                                        
+
                                     </div>
                                 </div>
                             </div>
