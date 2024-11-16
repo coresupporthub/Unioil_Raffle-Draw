54b00f1065c41f7475eaeaab36ee6db33a295f05
RheyanJohn <info@coresupporthub.com>
2024-11-16 23:30:51 +0800

    ADD product list seeder

--- database/seeders/ProductListSeeder.php
+++ database/seeders/ProductListSeeder.php
@@ -14,24 +14,24 @@ class ProductListSeeder extends Seeder
     public function run(): void
     {
         $products = [
-            ['IDEMITSU 4T SL/MA 20W-50 1L', 'Fully Synthetic', 2],
-            ['IDEMITSU 4T SL/MA 10W-40 SS 1L', 'Fully Synthetic', 2],
-            ['IDEMITSU 4T 10W-40 SL/MB S-S 1L', 'Fully Synthetic', 2],
-            ['IRG3 10W-40 MA 1L', 'Fully Synthetic', 2],
-            ['IRG3 10W-40 MB 1L', 'Fully Synthetic', 2],
-            ['IRG5 10W-30 MB 1L ', 'Fully Synthetic', 2],
-            ['IRG7 10W-40 MA2 1L', 'Fully Synthetic', 2],
-            ['UN MOTOSPORT 200 SF/40 800ml', 'Fully Synthetic', 2],
-            ['UN MOTOSPORT 200 SF/40 800ml', 'Fully Synthetic', 2],
-            ['UN MOTOSPORT 500 20W-40 SL MA 1L', 'Semi Synthetic', 2],
-            ['UN MOTOSPORT 500 20W-40SL/MA 800ml', 'Semi Synthetic', 2],
-            ['UN MOTOSPORT 700 10W40 SN/MA2 1L', 'Semi Synthetic', 2],
-            ['UN MOTOSPORT 4T SCOOTER 800ml', 'Semi Synthetic', 2],
-            ['UN SCOOTER 500 20W40 SL MB 1L', 'Semi Synthetic', 2],
-            ['UN SCOOTER 700 10W40 SL MB (SS) 1L', 'Semi Synthetic', 2],
-            ['UN SCOOTER 1000 10W40 SL MB (FS) 1L', 'Semi Synthetic', 2],
-            ['UN SCOOTER 1000 10W40 SL MB (FS) 800ml', 'Semi Synthetic', 2],
-            ['UN MOTOSPORT SCOOTER GEAR OIL BUNDLE', 'Semi Synthetic', 2]
+            ['ID 4T SL/MA 20W-50  1L', 'MIN', 1],
+            ['ID 4T SL/MA 10W-40 SS 1L', 'SS', 1],
+            ['ID 4T 10W-40 SL/MB SS 1L', 'SS', 1],
+            ['IRG3 10W-40 MA 1L', 'SS', 1],
+            ['IRG3 10W-40 MB 1L', 'SS', 1],
+            ['IRG5 10W-30 MB 1L ', 'FS', 2],
+            ['IRG7 10W-40 MA2 1L', 'FS', 2],
+            ['UN MOTOSPORT 200 SF/40 800ml', 'MIN', 1],
+            ['UN MOTOSPORT 200 SF/40 1L', 'MIN', 1],
+            ['UN MOTOSPORT 500 20W-40 SL/MA/MA-2 1L', 'MIN', 1],
+            ['UN MOTOSPORT 500 20W-40SL/MA/MA 800ml', 'MIN', 1],
+            ['UN MOTOSPORT 700 10W40 SN/MA-2 1L', 'SS', 1],
+            ['UN MOTOSPORT 4T SCOOTER 800ml', 'MIN', 1],
+            ['UN SCOOTER 500 20W40 SL MB 1L', 'MIN', 1],
+            ['UN SCOOTER 700 10W40 SL MB (SS) 1L', 'SS', 1],
+            ['UN SCOOTER 1000 10W40 SL MB (FS) 1L', 'FS', 2],
+            ['UN SCOOTER 1000 10W40 SL MB (FS) 800ml', 'FS', 2],
+            ['UN MOTOSPORT SCOOTER GEAR OIL BUNDLE', 'FS', 2]
         ];
 
 
