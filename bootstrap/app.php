d5fe93368d8728b960d9357a90c331f3ff114bd8
RheyanJohn <info@coresupporthub.com>
2025-01-27 16:49:05 +0800

    Update header

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Console\Scheduling\Schedule;
 use App\Models\User;
 use Illuminate\Support\Facades\Artisan;
+use App\Http\Middleware\SecurityHeadersMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(Spatie\Csp\AddCspHeaders::class);
+        $middleware->append(SecurityHeadersMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
