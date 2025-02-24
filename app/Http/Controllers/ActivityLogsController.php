1c79e33841a30489a2072a55b5d31dd3857ed698
RheyanJohn <rjvblanco.chmsu@gmail.com>
2025-02-25 04:58:06 +0800

    Fix logs

--- app/Http/Controllers/ActivityLogsController.php
+++ app/Http/Controllers/ActivityLogsController.php
@@ -7,10 +7,32 @@ use App\Models\ActivityLogs;
 use Illuminate\Http\JsonResponse;
 class ActivityLogsController extends Controller
 {
-    public function list(): JsonResponse{
-        $logs = ActivityLogs::join('users', 'users.id', '=', 'activity_logs.user_id')->select('activity_logs.*', 'users.name')->get();
+    public function list(Request $request): JsonResponse{
+        $start = $request->input('start', 0);
+        $length = $request->input('length', 10);
+        $search = $request->input('search')['value'];
 
-        return response()->json(['success'=> true, 'logs'=> $logs]);
+        $query = ActivityLogs::join('users', 'users.id', '=', 'activity_logs.user_id')->select('activity_logs.*', 'users.name')
+                ->orderBy('activity_logs.created_at', 'desc');
+
+        if (!empty($search)) {
+            $query->where('code', 'like', "%$search%")
+                ->orWhere('entry_type', 'like', "%$search%")
+                ->orWhere('status', 'like', "%$search%");
+        }
+
+        $totalRecords = ActivityLogs::count();
+
+        $filteredRecords = $query->count();
+
+        $logs = $query->skip($start)->take($length)->get();
+
+        return response()->json([
+            'draw' => intval($request->input('draw')),
+            'recordsTotal' => $totalRecords,
+            'recordsFiltered' => $filteredRecords,
+            'data' => $logs
+        ]);
     }
 
     public function details(string $id): JsonResponse{
